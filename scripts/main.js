4a9aa5ad6ce0c463e8db12f4662ae63458ba7465
Gislaine Martins <50420562+GislaineMartins@users.noreply.github.com>
2022-04-28 12:01:57 -0300

    adicionando arquivos

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+let minhaImagem = document.querySelector('img');
+
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+    if(meuSrc === 'imagens/firefox.webp') {
+      minhaImagem.setAttribute ('src','imagens/firefox2.jpg');
+    } else {
+      minhaImagem.setAttribute ('src','imagens/firefox.webp');
+    }
+}
+
+let meuBotao = document.querySelector('button');
+let meuCabecalho = document.querySelector('h1');
+
+function defineNomeUsuario() {
+  let meuNome = prompt('Por favor, digite seu nome.');
+  if(!meuNome || meuNome === null) {
+    defineNomeUsuario();
+  } else {
+    localStorage.setItem('nome', meuNome);
+    meuCabecalho.innerHTML = 'Mozilla é legal, ' + meuNome;
+  }
+}
+
+if(!localStorage.getItem('nome')) {
+  defineNomeUsuario();
+} else {
+  let nomeGuardado = localStorage.getItem('nome');
+  meuCabecalho.textContent = 'Mozilla é legal, ' + nomeGuardado;
+}
+
+meuBotao.onclick = function() { 
+    defineNomeUsuario();
+}
+
+
